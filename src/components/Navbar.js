75c934231348e2e0e12a3980cf0283a5edc2bffa
Mohd Zubair Ahmed <100506226+Zubair7102@users.noreply.github.com>
2024-08-18 20:32:35 +0530

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -8,7 +8,7 @@ const Navbar = () => {
   const handleClick = () => setNav((prevState) => !prevState);
 
   return (
-    <div className="fixed w-full h-[80px] flex justify-between items-center px-4 bg-[#0a192f] text-gray-300">
+    <div className="fixed w-full h-[80px] flex justify-between items-center px-4 bg-[#0a192f] text-gray-300 z-[10]">
       <div>
         <img src={MZA} alt="Logo" className="w-[50px]" />
       </div>
