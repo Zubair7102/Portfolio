0afda3dd30abc6330b1e30d7de790fdfc1499c63
Mohd Zubair Ahmed <jubaira646@gmail.com>
2025-04-12 22:54:37 +0530

    Resume Update

--- src/components/Home.js
+++ src/components/Home.js
@@ -56,7 +56,7 @@ const Home = () => {
             </button>
           </Link>
           <a
-            href="https://drive.google.com/file/d/1UBbie5JIifIv_ukwUlGJeg4nSA89wEQq/view?usp=sharing"
+            href="https://drive.google.com/file/d/16GAopsjAHauF-dJHeJ4eEMI5IFRWCa2i/view?usp=sharing"
             target="_blank"
             rel="noopener noreferrer"
           >
